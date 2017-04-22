e75ca5fa8df2fd4bb280d51f0358da8c1d608ce9
cyril.ruedin <cyril.ruedin@he-arc.ch>
2017-04-22 17:55:02 +0200

    lolilol

---
+++ src/main/java/app/WebSecurityConfig.java
@@ -0,0 +1,7 @@
+package app;
+
+/**
+ * Created by nolvulon on 22.04.2017.
+ */
+public class WebSecurityConfig {
+}
---
+++ src/main/java/app/services/SecurityService.java
@@ -0,0 +1,7 @@
+package app.services;
+
+/**
+ * Created by nolvulon on 22.04.2017.
+ */
+public class SecurityService {
+}
