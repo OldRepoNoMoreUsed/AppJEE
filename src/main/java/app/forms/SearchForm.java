cb380e9c951246b2fd989673608242086f8c5e06
SommerNicolas <nicolas.sommer@he-arc.ch>
2017-04-24 12:06:41 +0200

    search start

--- src/main/java/app/forms/SearchForm.java
+++ src/main/java/app/forms/SearchForm.java
@@ -7,26 +7,14 @@ import javax.validation.constraints.Size;
  * Created by mega- on 22.04.2017.
  */
 public class SearchForm {
-    @Size(min=2, max=32, message = "Username size should be in the range 2...32")
-    private String username;
 
-    @NotNull
-    @Size(min=6, max=32, message = "Password size should be in the range 6...32")
-    private String password;
+    private String searchTerm;
 
-    public String getUsername(){
-        return username;
+    public String getSearchTerm(){
+        return searchTerm;
     }
 
-    public void setUsername(String username){
-        this.username = username;
-    }
-
-    public String getPassword(){
-        return password;
-    }
-
-    public void setPassword(String password){
-        this.password = password;
+    public void setSearchTerm(String searchTerm){
+        this.searchTerm = searchTerm;
     }
 }
